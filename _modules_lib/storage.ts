387d706c6623faaa1c4e5ef52156ccfe963bf16c
mikesacchettidj <mikesacchetti@gmail.com>
2025-08-13 01:01:50 +0200

    Delete _modules_lib directory

--- _modules_lib/storage.ts
+++
@@ -1,5 +0,0 @@
-
-export const load = <T>(key:string, fallback:T):T => {
-  try { const v = localStorage.getItem(key); return v ? JSON.parse(v) as T : fallback } catch { return fallback }
-}
-export const save = (key:string, value:any) => { try { localStorage.setItem(key, JSON.stringify(value)) } catch {} }
